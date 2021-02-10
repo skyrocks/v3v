5ba2fa9c2f68ed70c384dad0b4ba0ed3efb0f405
shilei <42784342@qq.com>
2021-02-10 08:46:32 +0800

    feat(project): 增加element-plus按需加载

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { ElButton, ElRow, ElLoading, ElMessage, ElMessageBox, ElNotification } from 'element-plus'
 
-createApp(App).mount('#app')
+const components: any[] = [ElButton, ElRow]
+
+const plugins: any = [ElLoading, ElMessage, ElMessageBox, ElNotification]
+
+const app = createApp(App)
+
+components.forEach((component: any) => {
+  app.component(component.name, component)
+})
+
+plugins.forEach((plugin: any) => {
+  app.use(plugin)
+})
+
+app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,32 @@
-import vue from '@vitejs/plugin-vue'
+// import vue from '@vitejs/plugin-vue'
 
-/**
- * https://vitejs.dev/config/
- * @type {import('vite').UserConfig}
- */
-export default {
-  plugins: [vue()]
-}
+// /**
+//  * https://vitejs.dev/config/
+//  * @type {import('vite').UserConfig}
+//  */
+// export default {
+//   plugins: [vue()]
+// }
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vitePluginImp from 'vite-plugin-imp'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'element-plus',
+          style: (name) => {
+            return `element-plus/lib/theme-chalk/${name}.css`
+          }
+        }
+      ]
+    })
+  ],
+  server: {
+    port: 3008
+  }
+})
